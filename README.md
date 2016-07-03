325fb01d67992824017688f90b72e869c1eca07a
Michael Durrant <m2@snap2web.com>
2016-07-03 11:33:21 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# basic_app
+
+This is the framework template for a rspec-capybara project
+It can be used to start testing any publically available web site.
---
+++ spec/foo_spec.rb
@@ -0,0 +1,28 @@
+require 'rspec'
+require 'capybara'
+require 'capybara/rspec'
+require 'capybara/dsl'
+
+Capybara.run_server = false
+Capybara.default_driver = :selenium
+Capybara.app_host = 'http://www.google.com'
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+describe 'Loads up correctly', :type => :feature do
+  before :each do
+    Capybara.current_driver = :chrome
+  end
+  it 'Has the expected search input field' do
+    visit('http://www.google.com')
+    search_button_on_page=find(:css, "input[aria-label='Search']")
+    expect(search_button_on_page).to be_true
+  end
+  it 'Shows results' do
+    visit('http://www.google.com')
+    find(:css, "input[aria-label='Search']").set("123")
+    expect(page).to have_content 'results'
+  end
+end
