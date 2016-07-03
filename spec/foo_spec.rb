bdc1a58640687adb8211e2ee6f90f23e48fd39b9
Michael Durrant <m2@snap2web.com>
2016-07-03 17:13:05 -0400

    Multiple Domains

--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -11,20 +11,47 @@ Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
-root='/'
 
 describe 'Loads up correctly', :type => :feature do
   before :each do
     Capybara.current_driver = :chrome
   end
-  it 'Has the expected search input field' do
-    visit( root )
-    search_button_on_page=find(:css, "input[aria-label='Search']")
-    expect(search_button_on_page).to be_true
+  describe "For Google pages" do
+
+    before :each do
+      root='/'
+    end
+    it 'Has the expected search input field' do
+      visit( @root )
+      search_button_on_page=find(:css, "input[aria-label='Search']")
+      expect(search_button_on_page).to be_true
+    end
+
+    it 'Shows results' do
+      visit( @root )
+      find(:css, "input[aria-label='Search']").set("123")
+      expect(page).to have_content 'results'
+    end
   end
-  it 'Shows results' do
-    visit( root )
-    find(:css, "input[aria-label='Search']").set("123")
-    expect(page).to have_content 'results'
+
+  describe 'For weather.com pages', :type => :feature do
+
+    before :each do
+      @http = page.driver.browser.send(:bridge).http.instance_variable_get(:@http)
+      @http.read_timeout = 120
+    end
+    after :each do
+      @http.read_timeout = 60
+    end
+    it 'Can then visit weather.com' do
+      visit( 'http://www.weather.com' )
+      expect(page).to have_content('WEATHER')
+    end
+
+    it 'Can see a search box at weather.com' do
+      visit( 'http://www.weather.com' )
+      expect(page).to have_selector('input')
+    end
+
   end
 end
