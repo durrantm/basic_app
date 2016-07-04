3f15a4eef09b751fca4b1484a3be34b8cf0dc000
Michael Durrant <m2@snap2web.com>
2016-07-04 06:28:37 -0400

    Complete specs

--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -5,26 +5,31 @@ require 'capybara/dsl'
 
 Capybara.run_server = false
 Capybara.default_driver = :selenium
-Capybara.app_host = 'http://www.google.com'
+Capybara.app_host = 'http://www.weather.com'
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
 root='/'
+login_page = "http://profile.weather.com/#/login"
 
 describe 'Loads up correctly', :type => :feature do
   before :each do
     Capybara.current_driver = :chrome
+    #Capybara.default_wait_time = 120
   end
-  it 'Has the expected search input field' do
+  it 'Has the expected login field' do
     visit( root )
-    search_button_on_page=find(:css, "input[aria-label='Search']")
-    expect(search_button_on_page).to be_true
+    login_button_on_page=find("span.login")
+    expect(login_button_on_page).to be
   end
-  it 'Shows results' do
-    visit( root )
-    find(:css, "input[aria-label='Search']").set("123")
-    expect(page).to have_content 'results'
+  it 'Allows a login to fail' do
+    visit ( login_page )
+    fill_in('email', with: 'm2@snap2web.com')
+    fill_in('password', with: 'blobbyblobby')
+    find('button#LogInBtn').click
+    expect(page).to have_selector 'form#loginForm'
   end
+
 end
