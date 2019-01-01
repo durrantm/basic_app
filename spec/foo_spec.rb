8c64cf5a1fe1d56cf90a0c694fbf4cdaddc0a865
Michael Durrant <m2@snap2web.com>
2018-12-31 21:40:14 -0500

    fix

--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -20,11 +20,11 @@ describe 'Loads up correctly', :type => :feature do
   it 'Has the expected search input field' do
     visit( root )
     search_button_on_page=find(:css, "input[aria-label='Search']")
-    expect(search_button_on_page).to be_true
+    expect(search_button_on_page).to be
   end
   it 'Shows results' do
     visit( root )
-    find(:css, "input[aria-label='Search']").set("123")
+    find(:css, "input[aria-label='Search']").set("123\n")
     expect(page).to have_content 'results'
   end
 end
